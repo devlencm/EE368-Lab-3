dbbaffdf47b008b7db372f32bea7414c9bab78aa
brandonameyer <meyerba@clarkson.edu>
2023-02-06 10:51:37 -0500

    renders stuff on page now, but need to render books.mustache template

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+
 $( document ).ready(function() {
     fetchData();
 });
 
 function fetchData() {
     //TODO: write AJAX code to fetch the data from service.php. On successful response, call renderbooks() and pass the received books data
+    // Using the core $.ajax() method
+    var $target=$("#target");
+
+    $.ajax({
+        url: "service.php",
+        dataType: "json",
+        success: function(booksData){
+            document.getElementById("target").innerHTML = '';
+            $.each(booksData, function(i,book){
+                $target.append(Mustache.render('{{author}}',book))
+            })
+        }
+    })
 }
 
 function renderBooks(booksData) {
     //TODO: use mustache to render booksData and books.mustache into #tarter
 }
-
